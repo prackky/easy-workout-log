57f97c14d2df28d5f2aa3f3e7653fc75d8f2fe64
Victor Parmar <victor.parmar@canoo.com>
2017-06-12 23:18:09 +0200

    Refactor generic content

--- src/components/log-workout/LogWorkout.js
+++ src/components/log-workout/LogWorkout.js
@@ -10,20 +10,7 @@ import LogWorkoutHelpModal from './LogWorkoutHelpModal';
 
 import logWorkoutActions from '../../modules/log-workout/logWorkoutActions';
 
-const weightHelpModalContent = [
-  'Weight in lbs.',
-  'To convert from kilograms to pounds, enter the weight in kgs and hit the convert button. To re-enable the button delete and re-add the exercise.'
-];
-
-const tempoHelpModalContent = [
-  'The speed of the exercise. For e.g. 101 means 1 second eccentric (negative), 0 second mid-point and 1 second concentric (positive).',
-                'Keep value at 101 (default) if unsure.'
-];
-
-const restHelpModalContent = [
-  'The rest in seconds between sets.',
-  'Setting this to 0 indicates that the next exercise is a super-set. Keep at 60 seconds (default) if unsure.'
-];
+import ewoloContent from '../../common/ewoloContent';
 
 const mapStateToProps = (state) => {
   return {logWorkout: state.user.logWorkout};
@@ -159,19 +146,19 @@ class LogWorkout extends Component {
           doSetShowHelp={this.props.doLogWorkoutSetShowTempoHelp}
           showHelp={this.props.logWorkout.showTempoHelp}
           title="Tempo"
-          content={tempoHelpModalContent} />
+          content={ewoloContent.tempoHelpModalContent} />
         
         <LogWorkoutHelpModal 
           doSetShowHelp={this.props.doLogWorkoutSetShowRestHelp}
           showHelp={this.props.logWorkout.showRestHelp}
           title="Rest"
-          content={restHelpModalContent} />
+          content={ewoloContent.restHelpModalContent} />
 
         <LogWorkoutHelpModal 
           doSetShowHelp={this.props.doLogWorkoutSetShowWeightHelp}
           showHelp={this.props.logWorkout.showWeightHelp}
           title="Weight"
-          content={weightHelpModalContent} />
+          content={ewoloContent.weightHelpModalContent} />
 
         <div className="container grid-480 section-content">
           <div className="columns">
