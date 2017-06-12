eaaefb11d191552c34dce884cc398c5e6147ca66
Victor Parmar <victor.parmar@canoo.com>
2017-06-13 00:52:57 +0200

    Refactor autocomplete to maintain internal state

--- src/components/generic/AutoCompleteMenu.js
+++ src/components/generic/AutoCompleteMenu.js
@@ -1,44 +1,68 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import autoCompleteSuggestions from '../../modules/generic/autoCompleteSuggestions';
 
-const AutoCompleteMenu = (props) => {
+class AutoCompleteMenu extends Component {
 
-  const {items, input, handleSelection} = props;
-  const suggestions = autoCompleteSuggestions(items, input);
-
-  if (suggestions.length === 0) {
-    return null;   
-  };
-
-  const handleCloseClick = () => {
-    // do nothing
+  constructor(props) {
+    super(props);
+    this.state = this.getStateFromProps(props);
   }
 
-  return (
-    <ul className="menu">
-      <li className="menu-item">
-        <button 
-        type="button" 
-        className="btn btn-clear float-right"
-        onClick={handleCloseClick}>
-        </button>
-      </li>
-      {suggestions.map((suggestion, index) => {
-        return (
-          <li className="menu-item" key={index}>
-            <a
-              onClick={(event) => {
-              event.preventDefault();
-              handleSelection(suggestion);
-            }}>
-              {suggestion}
-            </a>
-          </li>
-        );
-      })}
-    </ul>
-  );
-};
+  getStateFromProps(props) {
+    const {items, input, handleSelection} = props;
+    const suggestions = autoCompleteSuggestions(items, input);
+
+    return {handleSelection: handleSelection, suggestions: suggestions};
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState(this.getStateFromProps(nextProps));
+  }
+
+  handleCloseClick = () => {
+    const state = this.state;
+    const newState = {
+      ...this.state,
+      suggestions: []
+    };
+    this.setState(newState);
+  }
+
+  render() {
+    const self = this;
+
+    if (self.state.suggestions.length === 0) {
+      return null;
+    }
+
+    return (
+      <ul className="menu">
+        <li className="menu-item">
+          <button
+            type="button"
+            className="btn btn-clear float-right"
+            onClick={self.handleCloseClick}></button>
+        </li>
+        {self
+          .state
+          .suggestions
+          .map((suggestion, index) => {
+            return (
+              <li className="menu-item" key={index}>
+                <a
+                  onClick={(event) => {
+                  event.preventDefault();
+                  self.state.handleSelection(suggestion);
+                }}>
+                  {suggestion}
+                </a>
+              </li>
+            );
+          })}
+      </ul>
+    );
+  };
+}
 
 export default AutoCompleteMenu;
