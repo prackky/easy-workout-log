3bbc23052d0fabb5804a32a75bbde84c5ec50e3e
Victor Parmar <victor.parmar@canoo.com>
2017-06-13 22:57:04 +0200

    Fix api usage based on environment

--- src/common/ewoloConstants.js
+++ src/common/ewoloConstants.js
@@ -2,7 +2,7 @@ const ewoloConstants = Object.freeze({
   api: {
     apiKeyHeader: 'api-key',
     apiKey: 'ewolo-general',
-    url: process.env.NODE_ENV === 'production' ? 'http://localhost:9000/api/v1' : 'http://localhost:9000/api/v1'
+    url: process.env.NODE_ENV === 'production' ? '/api/v1' : 'http://localhost:9000/api/v1' // TODO: fix mocking of relative urls, https://github.com/wheresrhys/fetch-mock/issues/46
   },
   allExercises: [
     'Bench press',
