9adaba50115ccb893bd094ce14623d95e9c7bf8b
Victor Parmar <victor.parmar@canoo.com>
2017-06-17 00:47:43 +0200

    Switch api port

--- src/common/ewoloConstants.js
+++ src/common/ewoloConstants.js
@@ -3,7 +3,7 @@ const ewoloConstants = Object.freeze({
     authorizationHeader: 'authorization',
     apiKeyHeader: 'api-key',
     apiKey: 'ewolo-general',
-    url: process.env.NODE_ENV === 'production' ? '/api/v1' : 'http://localhost:9000/api/v1' // TODO: fix mocking of relative urls, https://github.com/wheresrhys/fetch-mock/issues/46
+    url: process.env.NODE_ENV === 'production' ? '/api/v1' : 'http://localhost:4000/api/v1' // TODO: fix mocking of relative urls, https://github.com/wheresrhys/fetch-mock/issues/46
   },
   storage: {
     authTokenKey: 'EWOLO-AUTH-TOKEN'
