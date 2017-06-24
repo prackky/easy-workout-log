ad66eb80c15303cd9155fb13230c4a814078f446
Victor Parmar <victor.parmar@canoo.com>
2017-06-24 20:53:45 +0200

    Cleanup component

--- src/components/auth/Logout.js
+++ src/components/auth/Logout.js
@@ -1,46 +1,9 @@
-import /*React,*/ {Component} from 'react';
-// import {Redirect} from 'react-router'; import {Link} from 'react-router-dom';
-// import {connect} from 'react-redux'; import UserNotificationBar from
-// '../notification/UserNotificationBar'; import loginActions from
-// '../../modules/login/loginActions';
+import React from 'react';
 
 import ewoloUtil from '../../common/ewoloUtil';
 import ewoloConstants from '../../common/ewoloConstants';
 
-/*
-const mapStateToProps = (state) => {
-  return {login: state.login, authToken: state.user.data.authToken};
-};
-
-const mapDispatchToProps = {
-  doLoginSetData: loginActions.loginSetData,
-  doLogin: loginActions.login
-};
-*/
-
-class Logout extends Component {
-
-  /*
-  handleBtnLoginClick = (event) => {
-    event.preventDefault();
-    this
-      .props
-      .doLogin();
-  };
-
-  handleEmailChange = (event) => {
-    console.log(event.target.value);
-    this
-      .props
-      .doLoginSetData(event.target.value, this.props.login.password, this.props.login.text);
-  };
-
-  handlePasswordChange = (event) => {
-    this
-      .props
-      .doLoginSetData(this.props.login.email, event.target.value, this.props.login.text);
-  };
-  */
+class Logout extends React.Component {
 
   componentDidMount() {
     ewoloUtil.removeObject(ewoloConstants.storage.authTokenKey);
@@ -51,10 +14,8 @@ class Logout extends Component {
   }
 
   render() {
-    // return (<Redirect to="/"/>);
     return null;
   }
 };
 
-// export default connect(mapStateToProps, mapDispatchToProps)(Logout);
 export default Logout;
