1801f33f42ab4cc12fb884b96c1e6f9edf661453
Victor Parmar <victor.parmar@canoo.com>
2017-07-23 21:32:06 +0200

    Add first blog entry!

---
+++ src/components/articles/Article.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Article = (props) => {
+
+  return (
+    <div className="container grid-960 section-content">
+      <div className="columns">
+        <div className="column col-12">
+          <h3 className="text-center">Blog</h3>
+
+          <p>
+          
+          </p>
+
+        </div>
+      </div>
+
+    </div>
+  );
+
+}
+
+export default Article;
---
+++ src/components/articles/HowToGainMuscle.js
@@ -0,0 +1,98 @@
+import React from 'react';
+
+import './Article.css';
+
+const HowToGainMuscle = (props) => {
+
+  return (
+    <div className="container grid-960 section-content blog">
+      <div className="columns">
+        <div className="column col-12">
+
+          <div className="tile margin-bottom-3rem">
+            <div className="tile-icon">
+              <figure className="avatar avatar-xl" data-initial="VP" style={{'background-color': '#5764c6'}}>
+                
+              </figure>
+            </div>
+            <div className="tile-content">
+              <p className="tile-title">Victor Parmar</p>
+              <p className="tile-subtitle">July 23, 2017</p>
+            </div>
+          </div>
+              
+          <h3>How to gain 20 pounds of muscle in 4 weeks</h3>
+
+<p>
+I will save you some time right away by letting you know that the next time you see a title that tells you how to lose or gain x amount of weight in very little time you can move on. Such articles are written by people who have achieved incredible results but the same approach will not work for the majority of the people due to the fact that everyone has a <i><u>different body type, diet and physical development</u></i>.
+</p>
+
+<p>
+Despite the above disclaimer, I do have some recommendations on how you can achieve some concrete results based on my personal experience and research. <i><u>I am 5'11 and I went from 122 lbs to 142 lbs in 4 weeks and then from 142 to 166 in another 5 weeks</u></i>.
+</p>
+
+<p>
+When I started working out I had no idea what I was doing. I was 20 years old and I just started working out with a friend who had a home gym. He himself had previously worked out with someone else in their home gym so he knew a little bit. We worked out regularly for a couple of months and our routine consisted of only working out our chest and biceps. <i><u>There was no talk of nutrition or tracking progress</u></i>. My friend was happy to continue with the same weights and I was happy to slowly increase weights which meant that I was getting stronger. I never saw much physical change though and after a couple of months I got demotivated and stopped working out.
+</p>
+
+<p>
+A year later when I was 21, I was lucky enough to reconnect with an old friend who was studying to become a nutritionist. He started coaching me and we started working out together. This time around, I started doing a lot of research online and we would discuss routines and diet together. With his help I went from skinnymalinky to someone whose clothes actually fit! I gained 40 lbs in the span of 6 months! It has been 13 years since I first started working out and I am finally comfortable enough to share my recommendations.
+</p>
+          <h4 className="margin-top-5rem">We are what we eat!</h4>
+
+<p>        
+This cannot be stressed enough: if you want to make a physical change, you need to start with your diet. <i><u>When working out, 90% of your effort should be on nutrition and 10% on the workout routine</u></i>. This is also why personal trainers exist. They ensure that you get your diet right and motivate you to work out.
+</p>
+
+<p>
+Figuring out what you need to eat to achieve your goals is a very difficult problem. I recommend getting professional help if you are starting out for the very first time. Someone local will be able to guide you on what is available in your area depending on your culinary preferences. There are also many resources online on what sorts of food you need when exercising. See this <a href="http://www.wikihow.com/Build-Muscle">WikiHow</a> article on building muscle.
+</p>
+
+<p>
+<i><u>Keeping a log of what you eat is highly recommended. Tracking my calorie intake has also helped me immensely.</u></i> For example during the periods where I was travelling and not working out, I cut down on the amount of food I ate to burn some body fat. However, when I was injured and not working out I ate as much as I would if I was exercising to help my body heal.
+</p>
+
+          <h4 className="margin-top-5rem">Establish goals and track your progress</h4>
+
+<p>
+  We all want to see results really quickly. In a way this is a good thing - progress motivates us and makes us want to achieve more. <i><u>Tracking progress also lets us evaluate what is working and what is not</u></i>. This is why I built <a href="https://ewolo.fitness">Ewolo</a>. I wanted an easy way to track my workouts and see how my shoulders are getting stronger.
+</p>
+
+          <h4 className="margin-top-5rem">The mindset</h4>
+
+<p>
+While goals and tracking progress is important, it is also important to have the right mindset. Before you start working out ask yourself a few questions: What if I only lost 5 lbs in the 4 weeks? Will I give up or will I change something and try again? Even more interesting is: <i><u>What if I achieved my goals and lost 20 lbs in 4 weeks? Will I stop exercising?</u></i>
+</p>
+
+<p>
+The right mindset is where you plan for more than just 4 weeks, 6 months or a year. You plan to change your lifestyle and habits. When I started working out I made a mental roadmap of 10 years. I knew that the first gains were easy beause I had never exercised before, but keeping myself fit was going to be difficult. Every year I would re-evaluate my goals and work towards them. Some years it was as simple as: "recover from shoulder injury" or "increase bench press to 165 lbs". Quite a few times life got in the way and I was never able to achieve my goals. This has not stopped me till now. I continue to work on towards my goal of 180 lbs. and even created <a href="https://ewolo.fitness">Ewolo</a> to keep myself motivated!
+</p>
+
+          <h4 className="margin-top-5rem">Our bodies are resistant to change</h4>
+
+<p>
+The human body is built to adapt. If you simply stop eating any food, your body starts producing more fat because it thinks that you won't be getting food anytime soon. It's like preparing for hibernation. Therefore if you're looking to lose weight the idea is to eat small meals but more frequently. This allows the body to start using up it's energy store which involves breaking down fat. 
+</p>
+
+<p>
+This is also the reason why you see a much higher rate of progress when you start but soon hit what is known as a &quot;plateau&quot; where your body gets used to the diet and routine. Changing eating habits every 6 months or so is very difficult for most of us and therefore we try to focus on the workout routine or the type of exercise. In my experience I have found that simply taking a month off to travel or focus on a sport/hobby helped me to reset and break some plateaus.          
+</p>
+
+<h4 className="margin-top-5rem">Closing notes</h4>
+
+<p>
+Please note I am not a professional trainer nor a nutritionist. You should ideally get professional advice before starting a workout program. The opionions expressed in this article constitute a good basis for research and learning more. <i><u>There are no shortcuts to improving your health</u></i>.
+</p>
+
+<p>
+I wish you all the very best.
+</p>
+        </div>
+      </div>
+
+    </div>
+  );
+
+}
+
+export default HowToGainMuscle;
