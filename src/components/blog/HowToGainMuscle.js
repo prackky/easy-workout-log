5986c8a6d57d40d04f00225a1655f9c66d6684db
Victor Parmar <victor.parmar@canoo.com>
2017-07-23 23:03:44 +0200

    Blog post improvements

--- src/components/blog/HowToGainMuscle.js
+++ src/components/blog/HowToGainMuscle.js
@@ -25,15 +25,15 @@ const HowToGainMuscle = (props) => {
           <h3>How to gain 20 pounds of muscle in 4 weeks</h3>
 
 <p>
-I will save you some time right away by letting you know that the next time you see a title that tells you how to lose or gain x amount of weight in very little time you can move on. Such articles are written by people who have achieved incredible results but the same approach will not work for the majority of the people due to the fact that everyone has a <i><u>different body type, diet and physical development</u></i>.
+I will save you some time right away by letting you know that the next time you see a title that tells you how to lose or gain x amount of weight in very little time you can move on. Such articles are written by people who have achieved incredible results but the same approach will not work for the majority of the people due to the fact that everyone has a <span className="highlight">different body type, diet and physical development</span>.
 </p>
 
 <p>
-Despite the above disclaimer, I do have some recommendations on how you can achieve some concrete results based on my personal experience and research. <i><u>I am 5'11 and I went from 122 lbs to 142 lbs in 4 weeks and then from 142 to 166 in another 5 weeks</u></i>.
+Despite the above disclaimer, I do have some recommendations on how you can achieve some concrete results based on my personal experience and research. <span className="highlight">I am 5'11 and I went from 122 lbs to 142 lbs in 4 weeks and then from 142 to 166 in another 5 weeks</span>.
 </p>
 
 <p>
-When I started working out I had no idea what I was doing. I was 20 years old and I just started working out with a friend who had a home gym. He himself had previously worked out with someone else in their home gym so he knew a little bit. We worked out regularly for a couple of months and our routine consisted of only working out our chest and biceps. <i><u>There was no talk of nutrition or tracking progress</u></i>. My friend was happy to continue with the same weights and I was happy to slowly increase weights which meant that I was getting stronger. I never saw much physical change though and after a couple of months I got demotivated and stopped working out.
+When I started working out I had no idea what I was doing. I was 20 years old and I just started working out with a friend who had a home gym. He himself had previously worked out with someone else in their home gym so he knew a little bit. We worked out regularly for a couple of months and our routine consisted of only working out our chest and biceps. <span className="highlight">There was no talk of nutrition or tracking progress</span>. My friend was happy to continue with the same weights and I was happy to slowly increase weights which meant that I was getting stronger. I never saw much physical change though and after a couple of months I got demotivated and stopped working out.
 </p>
 
 <p>
@@ -47,7 +47,7 @@ A year later when I was 21, I was lucky enough to reconnect with an old friend w
           <h4 className="margin-top-5rem">We are what we eat!</h4>
 
 <p>        
-This cannot be stressed enough: if you want to make a physical change, you need to start with your diet. <i><u>When working out, 90% of your effort should be on nutrition and 10% on the workout routine</u></i>. This is also why personal trainers exist. They ensure that you get your diet right and motivate you to work out.
+This cannot be stressed enough: if you want to make a physical change, you need to start with your diet. <span className="highlight">When working out, 90% of your effort should be on nutrition and 10% on the workout routine</span>. This is also why personal trainers exist. They ensure that you get your diet right and motivate you to work out.
 </p>
 
 <p>
@@ -55,19 +55,23 @@ Figuring out what you need to eat to achieve your goals is a very difficult prob
 </p>
 
 <p>
-<i><u>Keeping a log of what you eat is highly recommended. Tracking my calorie intake has also helped me immensely.</u></i> For example during the periods where I was travelling and not working out, I cut down on the amount of food I ate to burn some body fat. However, when I was injured and not working out I ate as much as I would if I was exercising to help my body heal.
+<span className="highlight">Keeping a log of what you eat is highly recommended. Tracking my calorie intake has also helped me immensely.</span> For example during the periods where I was travelling and not working out, I cut down on the amount of food I ate to burn some body fat. However, when I was injured and not working out I ate as much as I would if I was exercising to help my body heal.
 </p>
 
           <h4 className="margin-top-5rem">Establish goals and track your progress</h4>
 
 <p>
-  We all want to see results really quickly. In a way this is a good thing - progress motivates us and makes us want to achieve more. <i><u>Tracking progress also lets us evaluate what is working and what is not</u></i>. This is why I built <a href="https://ewolo.fitness">Ewolo</a>. I wanted an easy way to track my workouts and see how my shoulders are getting stronger.
+  We all want to see results really quickly. In a way this is a good thing - progress motivates us and makes us want to achieve more. <span className="highlight">Tracking progress also lets us evaluate what is working and what is not</span>. This is why I built <a href="https://ewolo.fitness">Ewolo</a>. I wanted an easy way to track my workouts and see how my shoulders are getting stronger.
+</p>
+
+<p>
+  If you are just starting out and need motivation, <span className="highlight">start small</span>. Start by calculating the calories in your next meal. Then move on to calculate the nutrition - break down the amount of protein, carbs and minerals. Make small achievable goals, for example &quot;no sugary drinks for the week&quot;. Achieving such small goals will lead to a snowball effect where you will have maximised your potential for progress even before you step into the gym!
 </p>
 
           <h4 className="margin-top-5rem">The mindset</h4>
 
 <p>
-While goals and tracking progress is important, it is also important to have the right mindset. Before you start working out ask yourself a few questions: What if I only lost 5 lbs in the 4 weeks? Will I give up or will I change something and try again? Even more interesting is: <i><u>What if I achieved my goals and lost 20 lbs in 4 weeks? Will I stop exercising?</u></i>
+While goals and tracking progress is important, it is also important to have the right mindset. Before you start working out ask yourself a few questions: What if I only lost 5 lbs in the 4 weeks? Will I give up or will I change something and try again? Even more interesting is: <span className="highlight">What if I achieved my goals and lost 20 lbs in 4 weeks? Will I stop exercising?</span>
 </p>
 
 <p>
@@ -84,10 +88,14 @@ The human body is built to adapt. If you simply stop eating any food, your body 
 This is also the reason why you see a much higher rate of progress when you start but soon hit what is known as a &quot;plateau&quot; where your body gets used to the diet and routine. Changing eating habits every 6 months or so is very difficult for most of us and therefore we try to focus on the workout routine or the type of exercise. In my experience I have found that simply taking a month off to travel or focus on a sport/hobby helped me to reset and break some plateaus.          
 </p>
 
+<p>
+  Don't settle into a routine. To stay motivated, <span className="highlight">keep setting the bar higher</span>. Also, keep an open mind and don't be afraid to reach out for help!
+</p>
+
 <h4 className="margin-top-5rem">Closing notes</h4>
 
 <p>
-Please note I am not a professional trainer nor a nutritionist. You should ideally get professional advice before starting a workout program. The opionions expressed in this article constitute a good basis for research and learning more. <i><u>There are no shortcuts to improving your health</u></i>.
+Please note I am not a professional trainer nor a nutritionist. You should ideally get professional advice before starting a workout program. The opionions expressed in this article constitute a good basis for research and learning more. <span className="highlight">There are no shortcuts to improving your health</span>.
 </p>
 
 <p>
