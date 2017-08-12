7ef586d16aeee3d5c4f6801820f86a407bfcf395
Victor Parmar <victor.parmar@canoo.com>
2017-08-12 22:25:55 +0200

    Update development url for testing

--- src/common/ewoloConstants.js
+++ src/common/ewoloConstants.js
@@ -9,7 +9,7 @@ const ewoloConstants = Object.freeze({
     apiKeyHeader: 'api-key',
     apiKey: 'ewolo-general',
     version: apiVersion,
-    url: process.env.NODE_ENV === 'production' ? '/api/' + apiVersion : 'http://localhost:4000/api/' + apiVersion // TODO: fix mocking of relative urls, https://github.com/wheresrhys/fetch-mock/issues/46
+    url: process.env.NODE_ENV === 'production' ? '/api/' + apiVersion : 'http://192.168.42.10:4000/api/' + apiVersion // TODO: fix mocking of relative urls, https://github.com/wheresrhys/fetch-mock/issues/46
   },
   storage: {
     authTokenKey: 'EWOLO-AUTH-TOKEN',
