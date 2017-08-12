e116fbc550a99e4386202893b6c79f2289cce648
Victor Parmar <victor.parmar@canoo.com>
2017-08-12 23:13:12 +0200

    Add app notification for standardized menus

--- src/services/appNotificationService.js
+++ src/services/appNotificationService.js
@@ -3,10 +3,15 @@ const appNotifications = [
     id: 'custom-weight-units',
     text: 'Ewolo 2.0.0: Ability to set default units for weight and choose between lbs and kgs when logging exercises.',
     showAll: true // TODO: add ability to show notifications per user
+  },
+  {
+    id: 'standardized-menus',
+    text: 'Ewolo 2.6.3: Dashboard progress filters + standardized menus, click to open and click to close.',
+    showAll: true // TODO: add ability to show notifications per user
   }
 ];
 
 export const getAppNotification = () => {
-  // return appNotifications[0];
-  return null;
+  return appNotifications[1];
+  // return null;
 }
