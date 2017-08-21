9aec3f139e11cc874c8bc6de18a2a9ed2b23ad61
Victor Parmar <victor.parmar@canoo.com>
2017-08-21 23:51:18 +0200

    Add social icons to footer

--- src/components/page/Footer.js
+++ src/components/page/Footer.js
@@ -8,35 +8,40 @@ const Footer = (props) => {
   return (
     <footer className="bg-gray">
       <section id="copyright" className="grid-footer grid-1280">
-        {/*
         <div className="columns">
-          <div className="column col-12">
-            &copy; 2017 Ewolo
-          </div>
-          <div className="column col-12">
-            <Link to="/privacy">Privacy</Link> &nbsp;
-            <Link to="/terms">Terms</Link> &nbsp;
-          </div>
-        </div>
-        <div className="columns">
-          <div className="column col-12">
-            Built with ❤ by&nbsp;<a href="https://smalldata.tech">SmallData</a>.
-          </div>
-        </div>
-        */}
+          <div className="column col-md-12 col-8">
+            <div className="r">
+              <a href="mailto:feedback@ewolo.fitness">Contact</a>
+              &nbsp;&#8226;&nbsp;<Link to="/change-log">Change log</Link>
+              &nbsp;&#8226;&nbsp;<a href="https://github.com/victorparmar/easy-workout-log">Github</a>
+            </div>
+            <div className="r">
+              Version {props.clientVersion}
+              &nbsp;/ {window.SERVER_VERSION === '__SERVER_VERSION__'
+                ? props.apiVersion
+                : window.SERVER_VERSION}
+            </div>
+            <div className="r">
+              By using this service you agree to the&nbsp;<Link to="/privacy">Privacy policy</Link>
+              &nbsp;and&nbsp;<Link to="/terms">Terms of service</Link>.
+            </div>
 
-        <div className="r">
-          <a href="mailto:feedback@ewolo.fitness">Contact</a> &#8226; <Link to="/change-log">Change log</Link> &#8226; <a href="https://github.com/victorparmar/easy-workout-log">Github</a>
+          </div>
+          <div className="column col-md-12 col-4">
+            <div className="r">
+              <a href="https://www.facebook.com/ewolo.fitness" className="social-icon">
+                <i className="fa fa-facebook-square" aria-hidden="true"></i>
+              </a>
+              <a href="https://twitter.com/EwoloFitness" className="social-icon">
+                <i className="fa fa-twitter-square" aria-hidden="true"></i>
+              </a>
+            </div>
+            <div className="r">
+              Built with&nbsp;<i className="fa fa-heart" aria-hidden="true"></i>&nbsp;by&nbsp;<a href="https://smalldata.tech">SmallData</a>.
+            </div>
+          </div>
         </div>
-        <div className="r">
-          By using this service you agree to the <Link to="/privacy">Privacy policy</Link> and <Link to="/terms">Terms of service</Link>. 
-        </div>
-        <div className="r">
-          Version {props.clientVersion} / {window.SERVER_VERSION === '__SERVER_VERSION__' ? props.apiVersion : window.SERVER_VERSION} {/*, Release Notes */}
-        </div>
-        <div className="r">
-          Built with ❤ by&nbsp;<a href="https://smalldata.tech">SmallData</a>.
-        </div>
+
       </section>
     </footer>
   );
